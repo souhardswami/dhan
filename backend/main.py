690f937a9419f923a0c6fe8bb14cf0efaff2416d
souhard <souhardswami987@gmail.com>
2022-10-30 13:01:43 +0530

    end point added

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from datetime import date
 
 app = FastAPI()
 
-@app.get("/stocks")
-def get_stock_prices():
+@app.get("/stocks/{name}/start/{start_date}/end/{end_date}")
+def get_stock_prices(name: str, start_date: date, end_date: date):
+    
     return {"jio": "1$", "relience": "2$", "twitter": "1$"}
